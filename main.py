3618cad268c3ae8a2b4fec65632b5b1d834ce1f3
defjoy <defjoy9@users.noreply.github.com>
2024-12-10 12:19:57 -0500

    code now generates random dot every frame

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import pygame
+import random
+
+
+def draw_random_circle(screen, color, radius, screen_width, screen_height):
+    x = random.randint(radius, screen_width - radius)
+    y = random.randint(radius, screen_height - radius)
+    return pygame.draw.circle(screen,color, (x, y), radius, 0)
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 running = True
 
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill("purple")
+    screen.fill("black")
+    draw_random_circle(screen, (0, 255, 0), 10, 1280, 720)
     pygame.display.flip()
 pygame.quit()
