0eaedb1111c770d765aa899fdbf2e666a9fdac56
defjoy <defjoy9@users.noreply.github.com>
2024-12-10 12:48:23 -0500

    changed random dot function to spawn different location when an action occurrs

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import pygame
 import random
 
 
-def draw_random_circle(screen, color, radius, screen_width, screen_height):
+def generate_random_position(screen_width, screen_height, radius):
     x = random.randint(radius, screen_width - radius)
     y = random.randint(radius, screen_height - radius)
-    return pygame.draw.circle(screen,color, (x, y), radius, 0)
+    return (x, y)
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 running = True
+
+radius = 10
+circle_color = (255, 0, 0)
+circle_position = generate_random_position(1280, 720, radius)
 
 
 while running:
@@ -18,6 +22,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill("black")
-    draw_random_circle(screen, (0, 255, 0), 10, 1280, 720)
+    pygame.draw.circle(screen, circle_color, circle_position, radius)
     pygame.display.flip()
 pygame.quit()
