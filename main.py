ecbf56bb342e2ff667dcba2896fed49672ab195b
defjoy <defjoy9@users.noreply.github.com>
2024-12-10 12:58:25 -0500

    game now has a snake body at starting point

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ def generate_random_position(screen_width, screen_height, radius):
     y = random.randint(radius, screen_height - radius)
     return (x, y)
 
+def draw_snake():
+    for segment in snake_body:
+        pygame.draw.circle(screen, (0, 255, 0), segment, radius)
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 running = True
 
 radius = 10
+snake_body = [(340, 360), (330, 360), (320, 360)]
 circle_color = (255, 0, 0)
 circle_position = generate_random_position(1280, 720, radius)
-
 
 while running:
     for event in pygame.event.get():
@@ -23,5 +26,9 @@ while running:
             running = False
     screen.fill("black")
     pygame.draw.circle(screen, circle_color, circle_position, radius)
+
+    # snake body
+    draw_snake()
+
     pygame.display.flip()
 pygame.quit()
